a2a80fbbbe7b73ed2516c7193511f7729f295c63
Vitaliy Kamelin <v.kamelin@gmail.com>
2026-06-07 22:04:28 +0500

    feat: Удален SubmitAnswerAction (main)
    Удален неиспользуемый SubmitAnswerAction и соответствующий маршрут.

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -13,7 +13,6 @@ use Slim\App;
 return static function (App $app): void {
     $app->get('/api/health', HealthAction::class);
     $app->post('/api/test/start', StartTestAction::class);
-    $app->post('/api/test/answer', SubmitAnswerAction::class);
     $app->post('/api/test/finish', FinishTestAction::class);
     $app->get('/api/result/{id}', GetResultAction::class);
     $app->get('/dashboard', DashboardAction::class);
